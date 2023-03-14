56821a7ec3f10a1775221850f9b0813d2f8de51d
LukaszButurla <104159778+LukaszButurla@users.noreply.github.com>
2023-03-14 14:59:10 +0100

    Add files via upload

--- waz.py
+++ waz.py
@@ -9,6 +9,8 @@ def waz():
     run = True
     zmiennaX = 400
     zmiennaY = 400
+    pozycjaWaz = [(zmiennaX, zmiennaY)]
+    dlugoscWaz = 1
     jablkoX = random.randint(0, 19) * 40
     jablkoY = random.randint(0, 19) * 40
     score = 0    
@@ -46,22 +48,28 @@ def waz():
         if zmiennaX > 760:
             zmiennaX = 0
 
+        pozycjaWaz.append((zmiennaX, zmiennaY))
+
+        if len(pozycjaWaz)>dlugoscWaz:
+            del pozycjaWaz[0]
 
             
 
-        ksztaltWaz = pygame.Rect((zmiennaX, zmiennaY), (40, 40))
-        pygame.draw.rect(oknoGry, (100, 100, 100), ksztaltWaz)
 
         if zmiennaX==jablkoX and zmiennaY==jablkoY:
             jablkoX = random.randint(0, 19) * 40
             jablkoY = random.randint(0, 19) * 40
             score += 1
+            dlugoscWaz +=1
 
         ksztaltJablko = pygame.draw.circle(oknoGry, (255, 0, 0), (jablkoX + 20, jablkoY + 20), 20)
         czcionka = pygame.font.SysFont("Arial", 25)
         tekst = czcionka.render("Zdobyte puknty: {}".format(score), 1, (150, 200, 75))
         oknoGry.blit(tekst, (10, 10))
 
+        for pos in pozycjaWaz[::-1]:
+            ksztaltWaz = pygame.Rect((pos[0], pos[1]), (40, 40))
+            pygame.draw.rect(oknoGry, (100, 100, 100), ksztaltWaz)
 
         pygame.display.update()
 
