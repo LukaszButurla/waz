1b647ff06368f6df4bda124ebfd474ad663a4c14
LukaszButurla <104159778+LukaszButurla@users.noreply.github.com>
2023-03-27 14:09:46 +0200

    Add files via upload

--- snake.py
+++ snake.py
@@ -1,6 +1,8 @@
+import pygame
+
 class WazKlasa:
     def __init__(self):
-        self.pozycje = [(300, 300)]
+        self.pozycje = [(400, 400)]
         self.dlugosc = 1
         self.kierunek = [1, 0]
         self.punkty = 0
@@ -16,4 +18,23 @@ class WazKlasa:
 
         zmiennaX = ostatniaPozycja[0]+self.kierunek[0]*40
         zmiennaY = ostatniaPozycja[1]+self.kierunek[1]*40
+        noweWspolrzedne = self.check_border(zmiennaX,zmiennaY)
+        self.pozycje.append(noweWspolrzedne)
+        if len(self.pozycje) > self.dlugosc:
+            del self.pozycje[0]
 
+    def check_border(self, zmiennaX, zmiennaY):
+        if zmiennaY < 0:
+            zmiennaY = 760        
+        if zmiennaY > 760:
+            zmiennaY = 0
+        if zmiennaX < 0:
+            zmiennaX = 760
+        if zmiennaX > 760:
+            zmiennaX = 0
+        return (zmiennaX, zmiennaY)
+
+    def snakeDraw(self, oknoGry):
+        for pos in self.pozycje[::-1]:
+            ksztaltWaz = pygame.Rect((pos[0], pos[1]), (40, 40))
+            pygame.draw.rect(oknoGry, (100, 100, 100), ksztaltWaz)
--- waz.py
+++ waz.py
@@ -1,20 +1,22 @@
 import pygame
 import random
-from snake import Waz
+from snake import WazKlasa
+
+obiektWaz = WazKlasa()
 
 def waz():
     pygame.init()
     oknoGry = pygame.display.set_mode((800, 800),0,32)
     pygame.display.set_caption("Snake")
     run = True
-    zmiennaX = 400
-    zmiennaY = 400
-    pozycjaWaz = [(zmiennaX, zmiennaY)]
-    dlugoscWaz = 1
+    # zmiennaX = 400
+    # zmiennaY = 400
+    # pozycjaWaz = [(zmiennaX, zmiennaY)]
+    # dlugoscWaz = 1
     jablkoX = random.randint(0, 19) * 40
     jablkoY = random.randint(0, 19) * 40
     score = 0    
-    kierunek = [1, 0]
+    # kierunek = [1, 0]
     
     while run:
         oknoGry.fill((0,0,0))
@@ -28,48 +30,51 @@ def waz():
 
                 match(zdarzenia.key):
                     case pygame.K_DOWN:
-                            kierunek = [0, 1]
+                            obiektWaz.setDirection([0, 1])
                     case pygame.K_LEFT:
-                            kierunek = [-1, 0]
+                            obiektWaz.setDirection([-1, 0])
                     case pygame.K_RIGHT:
-                            kierunek = [1, 0]
+                            obiektWaz.setDirection([1, 0])
                     case pygame.K_UP:
-                            kierunek = [0, -1]
+                            obiektWaz.setDirection([0, -1])
 
-        zmiennaX = zmiennaX+kierunek[0]*40
-        zmiennaY = zmiennaY+kierunek[1]*40
+        obiektWaz.snakeMove()
+        # zmiennaX = zmiennaX+kierunek[0]*40
+        # zmiennaY = zmiennaY+kierunek[1]*40
 
-        if zmiennaY < 0:
-            zmiennaY = 760        
-        if zmiennaY > 760:
-            zmiennaY = 0
-        if zmiennaX < 0:
-            zmiennaX = 760
-        if zmiennaX > 760:
-            zmiennaX = 0
+        # if zmiennaY < 0:
+        #     zmiennaY = 760        
+        # if zmiennaY > 760:
+        #     zmiennaY = 0
+        # if zmiennaX < 0:
+        #     zmiennaX = 760
+        # if zmiennaX > 760:
+        #     zmiennaX = 0
 
-        pozycjaWaz.append((zmiennaX, zmiennaY))
+        # pozycjaWaz.append((zmiennaX, zmiennaY))
 
-        if len(pozycjaWaz)>dlugoscWaz:
-            del pozycjaWaz[0]
+        # if len(pozycjaWaz)>dlugoscWaz:
+        #     del pozycjaWaz[0]
 
             
 
 
-        if zmiennaX==jablkoX and zmiennaY==jablkoY:
+        if obiektWaz.getHeadPosition()[0] ==jablkoX and obiektWaz.getHeadPosition()[1]==jablkoY:
             jablkoX = random.randint(0, 19) * 40
             jablkoY = random.randint(0, 19) * 40
             score += 1
-            dlugoscWaz +=1
+            obiektWaz.dlugosc += 1
 
         ksztaltJablko = pygame.draw.circle(oknoGry, (255, 0, 0), (jablkoX + 20, jablkoY + 20), 20)
         czcionka = pygame.font.SysFont("Arial", 25)
         tekst = czcionka.render("Zdobyte puknty: {}".format(score), 1, (150, 200, 75))
         oknoGry.blit(tekst, (10, 10))
 
-        for pos in pozycjaWaz[::-1]:
-            ksztaltWaz = pygame.Rect((pos[0], pos[1]), (40, 40))
-            pygame.draw.rect(oknoGry, (100, 100, 100), ksztaltWaz)
+        # for pos in pozycjaWaz[::-1]:
+        #     ksztaltWaz = pygame.Rect((pos[0], pos[1]), (40, 40))
+        #     pygame.draw.rect(oknoGry, (100, 100, 100), ksztaltWaz)
+
+        obiektWaz.snakeDraw(oknoGry)
 
         pygame.display.update()
 
